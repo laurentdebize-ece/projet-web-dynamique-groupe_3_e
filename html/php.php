e3c3b90d9813225b502950be27ad9b50699af7ee
Chaperon <axel.chaperon@edu.ece.fr>
2023-05-16 00:25:55 +0200

    amelioration connexion et rajout inscription

--- html/php.php
+++ html/php.php
@@ -2,7 +2,7 @@
 
 try {
     // Connexion à la base de données MySQL
-    $bdd = new PDO('mysql:host=localhost;dbname=myomnesbox;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=myomnesbox;charset=utf8', 'root', '');
     // Définition du mode d'erreur de PDO sur Exception
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
@@ -13,30 +13,30 @@ try {
 // Récupération des données du formulaire
 if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['statut'])) {
     $nom = $_POST['nom'];
+    $nomEnMajuscules = strtoupper($nom);
     $prenom = $_POST['prenom'];
+    $prenomEnMajusculePremiereLettre = ucfirst(strtolower($prenom));
     $adresse_mail = $_POST['email'];
     $mot_de_passe = $_POST['password'];
     $statut = $_POST['statut'];
 
     // Vérification que les champs obligatoires sont remplis
-    if (!empty($nom) && !empty($prenom) && !empty($adresse_mail) && !empty($mot_de_passe)) {
+    if (!empty($nomEnMajuscules) && !empty($prenomEnMajusculePremiereLettre) && !empty($adresse_mail) && !empty($mot_de_passe)) {
         // Requête SQL pour insérer les données dans la table utilisateur
-        $insertion = "INSERT INTO utilisateur (Nom, Prenom, Adresse_mail, Mot_de_passe, Statut, Panier) VALUES ('$nom', '$prenom', '$adresse_mail', '$mot_de_passe', '$statut',0)";
+        $insertion = "INSERT INTO _utilisateur (Nom, Prenom, Adresse_mail, Mot_de_passe, Statut, Panier) VALUES ('$nomEnMajuscules', '$prenomEnMajusculePremiereLettre', '$adresse_mail', '$mot_de_passe', '$statut',0)";
         $bdd->query($insertion);
 
         // Récupérer l'ID de l'utilisateur inséré
         $id_utilisateur = $bdd->lastInsertId();
 
         // Mettre à jour la colonne "panier" avec l'ID de l'utilisateur inséré
-        $update = "UPDATE utilisateur SET panier = $id_utilisateur WHERE ID_utilisateur = $id_utilisateur";
+        $update = "UPDATE _utilisateur SET panier = $id_utilisateur WHERE ID_utilisateur = $id_utilisateur";
         $bdd->query($update);
 
-        // Affichage d'un message de confirmation
-        echo "<h1>Résultats</h1>";
-        echo "<p>Le compte a été créé avec succès !</p>";
+        // Redirection vers la page de connexion
+        header("Location: ../php/Mon%20compte%20non%20connecte.php");
     } else {
-        echo "<h1>Erreur</h1>";
-        echo "<p>Tous les champs sont obligatoires.</p>";
+        header("Location: ../html/creer-compte.html");
     }
 } else {
     echo "<h1>Erreur</h1>";
