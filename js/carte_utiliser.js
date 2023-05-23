f6de8d11d6b68a5e39655efcaca8648046aff242
Theo.Fornier <theo.fornier@edu.ece.fr>
2023-05-23 23:35:25 +0200

    theo

--- js/carte_utiliser.js
+++ js/carte_utiliser.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     $('.formule-carte').click(function () {
+        $('input').css({visibility: 'hidden'});
         $(this).children("form").children().css({visibility: 'visible'});
  
     });
